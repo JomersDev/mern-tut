56d8b6c410851448c575419bd3501515b5d9611e
James <james.mcgrory135@gmail.com>
2023-05-05 20:47:34 +1000

    MERN Auth tut 7 completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+// function created to return token from jwt package
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
 
 // login user
 const loginUser = async (req, res) => {
@@ -7,12 +13,18 @@ const loginUser = async (req, res) => {
 
 // signup user
 const signupUser = async (req, res) => {
+
+  // destructure the email and password properties from the req body obect
   const { email, password } = req.body
 
   try {
+    // create a user with the req properties, signup function created in /models/userModel
     const user = await User.signup(email, password)
 
-    res.status(200).json({ email, user})
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({ email, token})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
