fd5b7d93f13b229aea3c788af0a209354ba8fce5
James <james.mcgrory135@gmail.com>
2023-05-07 11:20:28 +1000

    MERN Auth tut 15 completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect} from "react"
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from '../hooks/useAuthContext'
 
 // components
 import WorkoutDetails from '../components/WorkoutDetails'
@@ -8,19 +9,28 @@ import WorkoutForm from "../components/WorkoutForm"
 const Home = () => {
 
   const {workouts, dispatch } = useWorkoutsContext()
+  const {user} = useAuthContext()
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('http://localhost:4000/api/workouts')
+      const response = await fetch('http://localhost:4000/api/workouts', {
+        headers: {
+          'Authorization': `Bearer ${user.token}`
+        }
+      })
       const json = await response.json()
 
       if(response.ok) {
         dispatch({type:'SET_WORKOUTS', payload: json})
       }
+    } 
+    
+    // only try and fetch workouts if user has a value
+    if (user) {
+      fetchWorkouts()
     }
     
-    fetchWorkouts()
-  }, [dispatch])
+  }, [dispatch, user])
 
   return (
     <div className="home">
